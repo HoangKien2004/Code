29f82f005beb750a31d5b0aa69b7c284abb1ad3f
HoangKien2004 <hoangkientdc123@gmail.com>
2024-08-23 16:03:57 +0700

    LastCode

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerFollow : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = .3f;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+    }
+}
